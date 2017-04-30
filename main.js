da5b32a1ea028babcfd6ad976b82d1fd16a32223
mobicom777 <j.mobicom@gmail.com>
2017-04-30 21:14:47 +1000

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,4 @@ myPlayer = amp("azuremediaplayer", myOptions);
 myPlayer.src([
     { src: "http://mobicom.streaming.mediaservices.windows.net/900a3e1b-f403-4042-9c5d-c238049c9444/Best Places to Visit in Sri Lanka, GoPlaces Sri Lanka  8 Wonders.ism/manifest", type: "application/vnd.ms-sstr+xml", streamingFormats: ["SMOOTH", "HLS","HLS-V3", "HLS-V4"]}, 
 ]);
+
